9ae360b6238b423eddc240077014de63b3e53c5a
anjumnr99 <anjumanowara99@gmail.com>
2023-10-18 18:26:55 +0600

    banner section is completed with responsive design

--- src/Componentes/Banner.jsx
+++ src/Componentes/Banner.jsx
@@ -16,18 +16,18 @@ const Banner = () => {
             <div className="relative">
                 <div className="absolute block">
                     <div className="flex h-[100vh]  ">
-                        <img data-aos="fade-up"  data-aos-offset="300" data-aos-easing="ease-in-sine" className="h-full w-[60%]" src="https://i.ibb.co/ZTSXH76/seductive-colorful-lipstick-colors-lip-care-coloring-lipstick-sexiness-generative-ai-210687-2103.jpg" alt="" />
+                        <img data-aos="fade-up"  data-aos-offset="300" data-aos-easing="ease-in-sine" className="h-full w-full md:w-[60%]" src="https://i.ibb.co/ZTSXH76/seductive-colorful-lipstick-colors-lip-care-coloring-lipstick-sexiness-generative-ai-210687-2103.jpg" alt="" />
 
-                        <img data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-offset="300" className="h-full w-[40%]" src="https://i.ibb.co/x7V0Hrv/creative-cosmetics-composition-advertisement-commercial-photoshoot-950002-26839.jpg" alt="" />
+                        <img data-aos="fade-down" data-aos-easing="ease-in-sine" data-aos-offset="300" className="h-full hidden md:block w-[40%]" src="https://i.ibb.co/x7V0Hrv/creative-cosmetics-composition-advertisement-commercial-photoshoot-950002-26839.jpg" alt="" />
 
 
                     </div>
                 </div>
 
-                <div data-aos="fade-left" data-aos-offset="300"
-                    data-aos-easing="ease-in-sine" data-aos-duration="1000" className="w-[40%]  bg-transparent p-6 absolute top-44 right-64 text-right">
+                <div data-aos="fade-left"
+                    data-aos-easing="ease-in-sine"  className=" md:w-[40%]  bg-transparent p-6 absolute top-32 lg:top-44 right-0 md:right-44 lg:right-64 text-right">
 
-                    <h1 className="mb-5 text-8xl text-white font-bold">Look beautiful enchanting</h1>
+                    <h1 className="mb-5 text-5xl  lg:text-7xl text-white font-bold">Look beautiful enchanting</h1>
                     <p className="mb-5 italic text-white">Beautiful with a charming look and style of your own choice </p>
                 </div>
 
